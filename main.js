cefdf450ebc3bdc774905a5942e786f4d3d2af2c
varek-l1d100 <varek.pro@gmail.com>
2020-01-25 22:26:45 +0100

    Ajout de quelques phrase

--- main.js
+++ main.js
@@ -7,7 +7,14 @@ client.on("ready", () => {
 });
 
 client.on("message", msg => {
-  if (msg.content.startsWith(PREFIX + "ping")) msg.channel.send("Pong!");
+  if (msg.content.startsWith(PREFIX + " ping")) msg.channel.send("Pong!");
+  if (msg.content.startsWith(PREFIX + " pong")) msg.channel.send("Ping!");
+  if (msg.content.startsWith(PREFIX + " pingpong"))
+    msg.channel.send("C'est du tenis de table!");
+  if (msg.content.startsWith(PREFIX + " iva"))
+    msg.channel.send(
+      "Je suis Iva. Le bot développez par Varek. Je suis utile pour la modération, mais aussi te faire écouté des musiques sur youtube. N'hésite pas à m'utilisez. Et si tu rencontre un souci, contacte Ｖａｒｅｋ3459"
+    );
 
   if (msg.content.startsWith(PREFIX + "enfin"))
     msg.channel.send("Tu as réussi papa!");
