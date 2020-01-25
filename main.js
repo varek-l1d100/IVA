6ed4971cc29e3794acadca766b2498677be7520a
varek-l1d100 <varek.pro@gmail.com>
2020-01-25 21:33:12 +0100

    Mise en place du everyone and noteveryone

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
+const { Client } = require("discord.js");
+const client = new Client({ disableEveryone: true });
 
 client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`Connectée sous le nom ${client.user.tag}!`);
 });
 
 client.on("message", msg => {
-  if (msg.content === "ping") {
-    msg.reply("Pong!");
-  }
+  if (msg.content === "ping") msg.channel.send("Pong!");
+  if (msg.content === "everyone")
+    msg.channel.send("@everyone salut à tous!", { disableEveryone: false });
+  if (msg.content === "noteveryone")
+    msg.channel.send("@everyone (noteveryone) Salut à tous!");
 });
 
-client.login("NjcwMzY3NjAyMzQ1OTAyMDgx.Xiyehg.ixAi0lgO546e1yZnXN72QZNezKw");
+client.login("NjcwMzY3NjAyMzQ1OTAyMDgx.Xiyj3w.z3HBUiroFKf86EiEzmqm5nHmL7I");
